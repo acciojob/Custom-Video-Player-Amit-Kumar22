1261c48887d3c63756cab94c2583d22a92387e55
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-09-26 08:53:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,62 @@
-/* Edit this file */
-const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
-const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider');
+// Get references to elements
+const video = document.getElementById("player__video viewer");
+const playPauseButton = document.getElementById("player__button toggle");
+const progress = document.getElementById("progress__filled");
+const volumeSlider = document.getElementById("player__slider");
+const speedSlider = document.getElementById("speed");
+const skipBackwardButton = document.getElementById("skip-backward");
+const skipForwardButton = document.getElementById("skip-forward");
+
+// Add event listeners
+video.addEventListener("click", togglePlay);
+playPauseButton.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", updateProgress);
+volumeSlider.addEventListener("input", updateVolume);
+speedSlider.addEventListener("input", updateSpeed);
+skipBackwardButton.addEventListener("click", skipBackward);
+skipForwardButton.addEventListener("click", skipForward);
+progress.addEventListener("click", seek);
+
+// Define functions for the event listeners
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function updateButton() {
+    const icon = video.paused ? "Play" : "Pause";
+    playPauseButton.textContent = icon;
+}
+
+function updateProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progress.style.width = `${percent}%`;
+}
+
+function updateVolume() {
+    video.volume = volumeSlider.value;
+}
+
+function updateSpeed() {
+    video.playbackRate = speedSlider.value;
+}
+
+function skipBackward() {
+    video.currentTime -= 10; // Skip 10 seconds backward
+}
+
+function skipForward() {
+    video.currentTime += 10; // Skip 10 seconds forward
+}
+
+function seek(e) {
+    const seekTime = (e.offsetX / progress.clientWidth) * video.duration;
+    video.currentTime = seekTime;
+}
+
+// Additional functionality can be added based on your requirements.
